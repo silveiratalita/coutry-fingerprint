2ecb016144256292a6acd4bbcff97556cb4fe9c6
Talita <silveira.tacristina@gmail.com>
2025-02-28 10:49:45 -0300

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -4,11 +4,9 @@ export default {
 		const country = request.headers.get('cf-ipcountry') || 'Unknown';
 		const userAgent = request.headers.get('user-agent') || 'Unknown';
 
-		// Gerar um fingerprint simples baseado no User-Agent
 		const fingerprint = generateFingerprint(userAgent);
 
 		try {
-			// Inserir dados no banco D1
 			await env.DB.prepare(`INSERT INTO acessos (pais, fingerprint) VALUES (?, ?)`).bind(country, fingerprint).run();
 
 			return new Response('Dados salvos com sucesso!', { status: 200 });
